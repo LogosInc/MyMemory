169f1481e72a27b4119d92f5560fb62e9a892c13
LogosInc <duomaxwell7032@gmail.com>
2021-03-13 00:21:01 +0800

    Finished Add menu options for game control

---
+++ app/src/main/java/com/example/mymemory/util/Constants.kt
@@ -0,0 +1,18 @@
+package com.example.mymemory.util
+
+import com.example.mymemory.R
+
+val DEFAULT_ICONS = listOf(
+        R.drawable.ic_bookmark,
+        R.drawable.ic_dating,
+        R.drawable.ic_face,
+        R.drawable.ic_flame,
+        R.drawable.ic_florist,
+        R.drawable.ic_gift,
+        R.drawable.ic_heart,
+        R.drawable.ic_home,
+        R.drawable.ic_lighting,
+        R.drawable.ic_moon,
+        R.drawable.ic_plane,
+        R.drawable.ic_school
+)
